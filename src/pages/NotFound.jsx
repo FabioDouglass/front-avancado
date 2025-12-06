f94494bea04c301eea0e19ca844af9e05732c757
Fabio Sousa <fabio.sousa@g4flex.com.br>
2025-12-06 17:17:49 -0300

    ajustes no not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
+import "./NotFound.css";
 export default function NotFound() {
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
+    <div className="notfound-container">
       <h1>404 - Página não encontrada</h1>
       <p>A URL acessada não existe.</p>
-      <a href="/">Voltar ao início</a>
+
+      <a href="/" className="back-button">
+        Voltar ao início
+      </a>
     </div>
   );
 }
