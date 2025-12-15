288d615551c70543cabe91c735825408b8d7e8d0
Fabio Sousa <fabio.sousa@g4flex.com.br>
2025-12-15 09:10:24 -0300

    aumentando data do home

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -3,6 +3,17 @@ const popularAlbums = [
   { id: "a002", media: 4.5 },
   { id: "a003", media: 3.9 },
   { id: "a004", media: 5.0 },
+  { id: "a005", media: 4.8 },
+  { id: "a006", media: 4.5 },
+  { id: "a007", media: 3.9 },
+  { id: "a008", media: 5.0 },
+  { id: "a009", media: 4.8 },
+  { id: "a010", media: 4.5 },
+  { id: "a011", media: 3.9 },
+  { id: "a012", media: 5.0 },
+  { id: "a013", media: 4.8 },
+  { id: "a014", media: 4.5 },
+  { id: "a015", media: 3.9 },
 ];
 
 const topRatedAlbums = [
